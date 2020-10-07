77b06384d9c7bdb3b8738ffaad872f369c3b3a02
danschae <danschae@gmail.com>
2020-10-07 22:55:57 +0000

    implement error features

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,22 +10,31 @@ $(document).ready(function() {
     event.preventDefault();
 
     let inputtedTweet = $("#tweet-text").val();
-    if (inputtedTweet === "" || inputtedTweet.length > 140) {
-      alert("errorrr!");
+    if (inputtedTweet === "") {
+      $(".error").empty();
+      $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error!!: Please make sure to not send empty tweets!<br> <i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000)
       $("form")[0].reset();
-    } else {
-    $.ajax({
-      url: "/tweets/",
-      method: "POST",
-      data: $(this).serialize()
-    })
-    .then( function(tweet) {
-      renderTweets([tweet])
-      $("form").trigger("reset")
-      $("#tweet-text").keyup();
-    })
+    }
+    if (inputtedTweet.length > 140) {
+      $(".error").empty();
+      $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error!!: Please make sure to not exceed character limit!<br> <i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000);
+      $("form")[0].reset();
+    }
+
+     else {
+      $.ajax({
+        url: "/tweets/",
+        method: "POST",
+        data: $(this).serialize()
+      })
+      .then( function(tweet) {
+        $(".error").empty();
+        renderTweets([tweet])
+        $("form").trigger("reset")
+        $("#tweet-text").keyup();
+      })
     .catch(error => console.log(error))
-  }
+    }
   })
 
   loadTweets();
