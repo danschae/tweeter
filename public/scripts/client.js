3c8e7e4c108c08b14ed3a58dc9b6f38cfcdb4ccd
danschae <danschae@gmail.com>
2020-10-08 22:46:42 +0000

    add comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,41 +5,34 @@
  */
 
 $(document).ready(function() {
-  $( "form" ).submit(function(event) {
+  $("form").submit(function(event) {
 
     event.preventDefault();
-
+    // input validation, if an error a message will be sent
     let inputtedTweet = $("#tweet-text").val();
     if (inputtedTweet === "") {
       $(".error").empty();
-      $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error: Please make sure to not send empty tweets!!<br> <i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000)
-      $("form")[0].reset();
-    }
-    if (inputtedTweet.length > 140) {
+      $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error: Please make sure to not send empty tweets!!<i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000);
+      $("form").trigger("reset");
+    } else if (inputtedTweet.length > 140) {
       $(".error").empty();
-      $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error: Please make sure not to exceed the character limit!<br> <i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000);
-      $("form")[0].reset();
-    }
-
-     else {
-      $.ajax({
+      $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error: Please make sure not to exceed the character limit!<i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000);
+    // the tweet will go as continue, the $("#tweet-text").keyup() method is used to reset the counter
+    } else {
+    $.ajax({
         url: "/tweets/",
         method: "POST",
         data: $(this).serialize()
       })
-      .then( function(tweet) {
+      .then(function (tweet) {
         $(".error").empty();
-        renderTweets([tweet])
+        renderTweets([tweet]);
         $("form").trigger("reset")
         $("#tweet-text").keyup();
       })
-    .catch(error => console.log(error))
+      .catch(error => console.log(error))
     }
   })
-
+  // tweets are loaded onto the screen when the DOM is initialized
   loadTweets();
-
-})
-
-
-
+});
