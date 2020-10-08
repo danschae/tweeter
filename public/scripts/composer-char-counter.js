770acf202ba8e52bef738e19207e1b4a278633e4
danschae <danschae@gmail.com>
2020-10-08 22:47:01 +0000

    add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     let countedChar = (140 - $(this).val().length);
     let displayedCounter = $(this).parent().find(".counter");
     displayedCounter.text(countedChar);
-
+    // conditional statement to see if the counter is negative, then the counter will turn red
     if (countedChar < 1) {
       displayedCounter.addClass("counterRed");
     } else {
