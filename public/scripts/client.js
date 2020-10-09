5722d605531c23e2a70ed5efc97fd36e2edc79ae
danschae <danschae@gmail.com>
2020-10-09 00:00:03 +0000

    use eslint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,15 +19,15 @@ $(document).ready(function() {
       $(".error").append(`<i class="fas fa-exclamation-triangle"></i>Error: Please make sure not to exceed the character limit!<i class="fas fa-exclamation-triangle"></i>`).hide().slideDown(1000);
     // the tweet will go as continue, the $("#tweet-text").keyup() method is used to reset the counter
     } else {
-    $.ajax({
+      $.ajax({
         url: "/tweets/",
         method: "POST",
         data: $(this).serialize()
       })
-      .then(function (tweet) {
+      .then(function(tweet) {
         $(".error").empty();
         renderTweets([tweet]);
-        $("form").trigger("reset")
+        $("form").trigger("reset");
         $("#tweet-text").keyup();
       })
       .catch(error => console.log(error))
