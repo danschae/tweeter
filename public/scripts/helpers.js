db2c426aaa09dfbfd787bb2f792dfd16158e24d2
danschae <danschae@gmail.com>
2020-10-09 00:00:24 +0000

    use eslint

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -3,7 +3,7 @@ const escape = (str) => {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML
-}
+};
 
 
 const createTweetElement = (tweet) => {
